1f83033f92db0f03faa22523f621a344f77ef0a7
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-27 12:25:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,10 +11,10 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.strip
   songs.each_with_index do |song, index|
-    if user_response.to_i == index + 1
+    if user_response.to_i == index + 1 || user_response == song
       puts "Playing #{song}"
-    elsif user_response == song
       puts "Playing #{song}"
+    elsif 
     end
   end
   puts message
