c4f16160a607fd16a331e1aa69d3d4d325f3aedc
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-27 12:13:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,8 +12,9 @@ def play(songs)
   songs.each_with_index do |song, index|
     if user_response.to_i == index + 1 || user_response == song
       puts "Playing #{song}"
-  else
-    puts "Invalid input, please try again"
+    else
+      puts "Invalid input, please try again"
+    end
   end
 end
 
