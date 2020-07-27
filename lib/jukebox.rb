73ede5e4dd712577e90d7f065f6324d232d4fe7b
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-27 12:10:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,7 +9,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.strip
-  if user_response.to_i >= 1 && user_response.to_i <= 
+  
+  
     puts "Playing #{}"
   else
     puts "Invalid input, please try again"
